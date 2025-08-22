3c4ddd4b1bd395713926584f1d133df70115fccf
Developer Nahida <ummenahida20@gmail.com>
2025-08-23 01:02:21 +0600

    setup redux rtk query

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,14 @@ import './index.css'
 import { RouterProvider } from 'react-router'
 import router from './Routes/router'
 import { Toaster } from 'sonner'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from './Redux/store'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />,
-    <Toaster position="top-center" richColors />
+    <ReduxProvider store={store}>
+      <RouterProvider router={router} />,
+      <Toaster position="top-center" richColors />
+    </ReduxProvider>
   </StrictMode>,
 )
