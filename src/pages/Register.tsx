a2415bac33946616a1052df4c44930230fae8277
Developer Nahida <ummenahida20@gmail.com>
2025-08-27 23:09:30 +0600

    added register

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,4 +1,3 @@
-import TravelRegister from "@/assets/images/register.jpg";
 import { Link } from "react-router";
 import Logo from "@/assets/Icons/logo";
 import { RegisterForm } from "@/components/Authentication/RegisterForm";
@@ -8,7 +7,7 @@ export default function Register() {
     <div className="grid min-h-screen lg:grid-cols-2">
       <div className="relative hidden bg-muted lg:block border">
         <img
-          src={TravelRegister}
+          src="/assets/images/register.jpg"
           alt="Image"
           className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.8]"
         />
